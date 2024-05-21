4a5944d27c24a343c5804d05312241c86441cff3
ArthurMAUGEEutc <arthur.maugee@etu.utc.fr>
2024-05-21 09:25:29 +0200

    Add files via upload

--- tp4.c
+++ tp4.c
@@ -14,3 +14,58 @@ T_Sommet *creerSommer(int element){
     return sommet;
 }
 
+int empty_abr(T_Arbre abr){
+    if (abr == NULL) {
+        return 0;
+    }else{
+        return 1;
+    }
+}
+
+T_Sommet *rechercherElement(T_Arbre abr, int element){
+    if (empty_abr(abr)){
+        printf("L'arbre est vide, le sommet n'a donc pas été trouvé ...\n");
+    }
+    else{
+        T_Sommet  *current = abr;
+        while(current != NULL){
+            if (element < current->inf){
+                current = current->Lson;
+            }
+            else if (element > current->sup){
+                current = current->Rson;
+            }
+            else {
+                return current;
+            }
+        }
+        if (current == NULL){
+            printf("Le sommet n'existe dans aucun intervalle ...\n");
+            return 0;
+        }
+    }
+}
+
+void afficher_intervalle(T_Sommet som){
+    int inf = som.inf;
+    int sup = som.sup;
+    for (inf;inf<=sup;inf++){
+        printf("[%d] ",inf);
+    }
+}
+
+void afficherSommet(T_Arbre abr){
+    if (empty_abr(abr) == 0){
+        printf("[%d:%d] ",abr->inf,abr->sup);
+        afficherSommet(abr->Lson);
+        afficherSommet(abr->Rson);
+    }
+}
+
+void afficherElements(T_Arbre abr){
+    if (empty_abr(abr) == 0){
+        afficher_intervalle(*abr);
+        afficherElements(abr->Lson);
+        afficherElements(abr->Rson);
+    }
+}
