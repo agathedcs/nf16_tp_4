779480529a8a4f72ab7e806a9f5ca3d05c16dc64
ArthurMAUGEEutc <arthur.maugee@etu.utc.fr>
2024-06-02 21:30:51 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -1,15 +1,87 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "tp4.h"
+#include "tp4.c"
 
 int main()
 {
+    char choix[7];
+    int nb_elements, element;
     T_Arbre abr = NULL;
-    abr = insererElement(abr, 10);
-    abr = insererElement (abr, 11);
-    abr = insererElement (abr, 20);
-    abr = insererElement (abr, 13);
-    abr = insererElement(abr, 12);
-    afficherSommet(abr);
-    return 0;
+    while (1){  //Permet de boucler à l'infini
+        printf("Entrez votre choix :\n\t"
+               "1 : Creer un arbre vide\n\t"
+               "2 : Construire un arbre de N elements\n\t"
+               "3 : Ajouter un element\n\t"
+               "4 : Afficher tout les sommets\n\t"
+               "5 : Afficher tout les éléments\n\t"
+               "6 : Supprimer un élément\n\t"
+               "7 : Afficher la taille en mémoire\n\t"
+               "8 : Quitter\n");
+        fgets(choix, sizeof(choix), stdin);
+        int choix_entier = atoi(choix);  //convertion de choix en entier
+        switch(choix_entier){
+            case 1:
+                if(abr != NULL){
+                    empty_abr_memory(abr);
+                    printf("L'arbre a été vidé\n");
+                    abr = NULL;
+                }
+                else{
+                    printf("L'arbre est déja vide\n");
+                }
+                break;
+
+            case 2:
+                printf("Création d'un arbre en cours...\nVeuillez rentrez le nombre d'élements que vous voulez ajoutez :\n");
+                scanf("%d",&nb_elements);
+                abr = insererElements(abr,nb_elements);
+                printf("Création effectué avec succès !\n");
+                break;
+
+            case 3:
+                printf("Rentrez l'élément à ajouter :\n");
+                scanf("%d",&element);
+                if (rechercherElement_messageless(abr,element) == NULL){
+                    abr = insererElement(abr,element);
+                    printf("Elément ajouté avec succès !\n");
+                }
+                else{
+                    printf("L'élément existe déjà\n");
+                }
+
+                break;
+
+            case 4:
+                if(empty_abr(abr)){
+                    printf("Arbre vide.\n");
+                }
+                else{
+                    printf("Sommets du graphe :\n");
+                    afficherSommet(abr,1);
+                }
+
+                break;
+
+            case 5:
+                printf("Eléments du graphe :\n");
+                afficherElements(abr,1);
+                break;
+
+            case 7:
+                printf("La taille en mémoire de l'arbre est égale à %zu bytes\n", Taille_Memoire(abr));
+                break;
+
+            case 8:
+                empty_abr_memory(abr);
+                printf("Sortie du programme\n");
+                exit(0);
+                break;
+
+            default:
+                printf("La valeur n'est pas accepté, Réeassayez\n");
+        }
+        printf("Entrez pour continuer .\n");
+        viderBuffer();
+    }
 }
--- tp4.h
+++ tp4.h
@@ -1,5 +1,9 @@
-#ifndef TP4_H_INCLUDED
-#define TP4_H_INCLUDED
+//
+// Created by Arthur Maugée on 02/06/2024.
+//
+
+#ifndef TP4_GIT_TP4_H
+#define TP4_GIT_TP4_H
 
 #include <stdlib.h>
 #include <stdio.h>
@@ -13,13 +17,17 @@ struct Sommet {
 };
 typedef T_Sommet* T_Arbre;
 
-T_Sommet *creerSommer(int element);
+T_Sommet *creerSommet(int element);
 T_Arbre insererElement (T_Arbre abr, int element);
+T_Arbre insererElements (T_Arbre abr, int nb_elements);
 int empty_abr(T_Arbre abr);
 T_Sommet *rechercherElement(T_Arbre abr, int element);
 T_Sommet *rechercherElement_messageless(T_Arbre abr, int element);
 void afficher_intervalle(T_Sommet som);
-void afficherSommet(T_Arbre abr);
-void afficherElements(T_Arbre abr);
+void afficherSommet(T_Arbre abr,int retour_ligne);
+void afficherElements(T_Arbre abr,int retour_ligne);
+int Taille_Memoire(T_Arbre abr);
+void viderBuffer();
+void empty_abr_memory(T_Arbre abr);
 
-#endif // TP4_H_INCLUDED
+#endif //TP4_GIT_TP4_H
