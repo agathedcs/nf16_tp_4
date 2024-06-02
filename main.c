198af20186e2037b39fda2d29afff5f32c5176d6
agathedcs <163671532+agathedcs@users.noreply.github.com>
2024-06-02 14:42:13 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "tp4.h"
 
 int main()
 {
-    printf("Hello world!\n");
+    T_Arbre abr = NULL;
+    abr = insererElement(abr, 10);
+    abr = insererElement (abr, 11);
+    abr = insererElement (abr, 20);
+    abr = insererElement (abr, 13);
+    abr = insererElement(abr, 12);
+    afficherSommet(abr);
     return 0;
 }
